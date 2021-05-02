62a0c571e1d17fd6867f58a095d284935f2eed36
Mustafa GÜNEŞ <gunes149@gmail.com>
2021-05-02 23:36:08 +0300

    Edit Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "Tom", targets: ["Tom"]),
     ],
     targets: [
-        .target(name: "Tom", path: "Source/Tom/Classes", linkerSettings: []),
+        .target(name: "Tom", path: "Source/Tom/Classes"),
         .testTarget(name: "TomTests", dependencies: ["Tom"]),
     ],
     swiftLanguageVersions: [.v5]
