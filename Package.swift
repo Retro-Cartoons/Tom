b2f32888eb2ef5576a22973b6fe25bb69608ad6e
Mustafa GÜNEŞ <gunes149@gmail.com>
2021-05-02 22:56:34 +0300

    Add swift package manager

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Tom",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "Tom", targets: ["Tom"]),
+    ],
+    targets: [
+        .target(name: "Tom", dependencies: []),
+        .testTarget(name: "TomTests", dependencies: ["Tom"]),
+    ]
+)
